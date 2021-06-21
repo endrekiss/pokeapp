ee45f08a321615d576d9c8ab3f0780b751222836
Endre Kiss <endre@screencode.at>
2021-06-21 10:51:42 +0200

    small API Service refactoring

--- src/Pokeapiservice.ts
+++ src/Pokeapiservice.ts
@@ -1,14 +1,12 @@
 export class Pokeapiservice {
     async getAbility(url: string) {
         const result = await fetch(url);
-        const datas = await result.json();
-        return datas
+        return await result.json()
     }
 
     async loadData(currentPage: number, perPage: number) {
         const url = 'https://pokeapi.co/api/v2/pokemon/?offset=' + ((currentPage - 1) * perPage) + '&limit=' + perPage
         const result = await fetch(url);
-        const datas = await result.json();
-        return datas
+        return await result.json()
     }
 }
